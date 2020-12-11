8fd8cd23d223a7497152100262bdfb649c40a823
Gary <66178774+gen8787@users.noreply.github.com>
2020-12-11 00:11:33 -0800

    comp report complete

--- 103_comp_report/main.js
+++ 103_comp_report/main.js
@@ -1,39 +1,30 @@
-//  M A T H   F U N C T I O N S
-function add(num1, num2) {
-    return num1 + num2;
-}
+// F U N C T I O N S   (E X T R A   C H A L L E N G E)
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
 
-function subtract() {
-    return num1 - num2;
-}
-
-function multiply() {
-    return num1 * num2;
-}
-
-function divide() {
-    return num1 / num2;
-}
+const refresh = () => location.reload();
 
 
 // M A I N
-let num1 = prompt("Enter a number."),
+let num1 = Number(prompt("Enter a number.")),
     num2 = null;
 
-if (isNaN(num1)) {
-    alert("Please enter a valid number!");
-    location.reload();
+if (!isNaN(num1)) {
+    num2 = Number(prompt("Enter another number."));
+
+    if (!isNaN(num2)) {
+        document.getElementById("add").innerHTML = (`${num1} + ${num2} = ${add(num1, num2)}`);
+        document.getElementById("subtract").innerHTML = (`${num1} - ${num2} = ${subtract(num1, num2)}`);
+        document.getElementById("multiply").innerHTML = (`${num1} * ${num2} = ${multiply(num1, num2)}`);
+        document.getElementById("divide").innerHTML = (`${num1} / ${num2} = ${divide(num1, num2)}`);
+    } else {
+        alert("Please enter a valid number!");
+        refresh();
+    };
+
 } else {
-    num2 = (prompt("Enter another number."));
-}
-
-if (isNaN(num2)) {
     alert("Please enter a valid number!");
-    location.reload();
-}
-
-
-add(num1, num2);
-subtract(num1, num2);
-multiply(num1, num2);
-divide(num1, num2);
+    refresh();
+};
